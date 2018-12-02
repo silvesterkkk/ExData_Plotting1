a32e9d3879a0efc00fdc367a5060a56fa0ed6e44
silvesterkkk <silvester.kim@gmail.com>
2018-12-02 23:59:49 +1100

    added all results

---
+++ plot1.R
@@ -0,0 +1,26 @@
+#read data
+df <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".", na.strings="?")
+
+#check if current memory is enough to read data
+# total rows * total columns * 8 (bytes) / 2^20 (in MB)
+t_row <- nrow(df)
+t_col <- ncol(df)
+req_memory <- (t_row * t_col * 8) / 2^20
+req_memory
+
+#subset data
+df_step1 <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+#convert date to date format
+df_step1$Date <- as.Date(df_step1$Date, format="%d/%m/%Y")
+df_step1$Global_active_power <- as.numeric(df_step1$Global_active_power)
+
+# Prevents Scientific Notation
+#df_step1[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+#export
+png("plot1.png", width=480, height=480)
+
+#plot
+hist(df_step1$Global_active_power, col="red", xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,40 @@
+#read data
+df <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".", na.strings="?")
+
+#check if current memory is enough to read data
+# total rows * total columns * 8 (bytes) / 2^20 (in MB)
+t_row <- nrow(df)
+t_col <- ncol(df)
+req_memory <- (t_row * t_col * 8) / 2^20
+req_memory
+
+#subset data
+df_step1 <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+#convert date to date format
+df_step1$Date <- as.Date(df_step1$Date, format="%d/%m/%Y")
+df_step1$Global_active_power <- as.numeric(df_step1$Global_active_power)
+df_step1$datetime <- as.POSIXct(paste(df_step1$Date, df_step1$Time))
+
+# Prevents Scientific Notation
+#df_step1[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+#export
+png("plot4.png", width=480, height=480)
+
+#plot
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(df_step1, {
+        plot(Global_active_power~datetime, type="l", 
+             ylab="Global Active Power", xlab="")
+        plot(Voltage~datetime, type="l", 
+             ylab="Voltage", xlab="datetime")
+        plot(Sub_metering_1~datetime, type="l", 
+             ylab="Energy sub metering", xlab="")
+        lines(Sub_metering_2~datetime,col='Red')
+        lines(Sub_metering_3~datetime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power~datetime, type="l", 
+             ylab="Global_rective_power",xlab="datetime")
+})
+dev.off()
